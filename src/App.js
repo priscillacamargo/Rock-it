91ac1a20b750a4e5c6183d589f2559b951b60fea
Priscilla Camargo <priscilla.camargo@pwc.com>
2020-02-14 12:48:34 +0100

    Add artists list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
+
+//Setting up Base Web components Libraries - easy comtumazible components with a flat design
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { LightTheme, BaseProvider, styled } from 'baseui';
+
+import ArtistsLists from './components/artists/ArtistsList';
+
+const engine = new Styletron();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <div className="body">
+          <header className="header">
+            <img src={logo} alt={'Rockstars IT Logo'} className="header__img"></img>
+            <h1>Rock-It NOW</h1>
+          </header>
+          <div>
+            <ArtistsLists></ArtistsLists>
+          </div>
+        </div>
+      </BaseProvider>
+    </StyletronProvider>
   );
 }
 
---
+++ src/components/artists/ArtistsList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ListItem, ListItemLabel } from 'baseui/list';
+
+const ArtistsList = () => {
+  return (
+    <ListItem>
+      <ListItemLabel>Label</ListItemLabel>
+    </ListItem>
+  );
+};
+
+export default ArtistsList;
